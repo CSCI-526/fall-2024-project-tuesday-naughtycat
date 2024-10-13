9cc6ad0a41a05e7dcf6c150a9f3578cec1d4e6e2
ShangrongXue <shangrong.xue@Outlook.com>
2024-10-13 00:22:37 -0700

    bullet speed and damage based on the gun type

--- Blend_Grow_Survive/Assets/Scripts/BulletAttack.cs
+++ Blend_Grow_Survive/Assets/Scripts/BulletAttack.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class BulletAttack : MonoBehaviour
 {
-    public float bullet_speed = 20f; 
+    public float bullet_speed; 
     public float max_distance = 20f;  
     public LayerMask enemy_layer_mask; 
 
     private Vector3 start_position; 
     ObjectGenerator generator;
+    private int gun = 0;
+    private Vector3 damage = new Vector3(0, 0, 0);
 
     void Start()
     {
+        switch (gun)
+        {
+            case 0:
+                bullet_speed = 20f;
+                damage.x = 0.5f;
+                damage.y = 0.5f;
+                damage.z = 0.5f;
+                break;
+            case 1:
+                bullet_speed = 30f;
+                damage.x = 0.7f;
+                damage.y = 0.7f;
+                damage.z = 0.7f;
+                break;
+            default:
+                bullet_speed = 40f;
+                damage.x = 1.0f;
+                damage.y = 1.0f;
+                damage.z = 1.0f;
+                break;
+        }
         start_position = transform.position;
         generator = ObjectGenerator.ins; 
     }
@@ -34,10 +57,15 @@ public class BulletAttack : MonoBehaviour
         {
             if (hit.collider.CompareTag("Enemy"))
             {
-                Destroy(hit.collider.gameObject);
-                generator.RemoveObject(hit.collider.gameObject, generator.created_enemies);
-                Destroy(gameObject);
-                GameManager.instance.AddEXP(5);
+                hit.collider.gameObject.transform.localScale -= damage;
+                if (hit.collider.gameObject.transform.localScale.x <= 0.0f)
+                {
+                    Destroy(hit.collider.gameObject);
+                    generator.RemoveObject(hit.collider.gameObject, generator.created_enemies);
+                    Destroy(gameObject);
+                    GameManager.instance.AddEXP(5);
+                }
+                
                 if (generator.created_enemies.Count == 0)
                 {
                     FindObjectOfType<PlayerEat>().WinGame();
--- Blend_Grow_Survive/Assets/Scripts/PlayerShoot.cs
+++ Blend_Grow_Survive/Assets/Scripts/PlayerShoot.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Diagnostics;
+using TMPro;
 using UnityEngine;
+
 
 public class PlayerShoot : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     public GameObject bullet; 
     public Transform bullet_start_point; 
-    public float bullet_speed = 20f;
+    //public float bullet_speed = 20f;
     ObjectGenerator generator;
     private bool has_shot = false; // To ensure player can only shoot once
     private Vector2 mouse_position;
     private Vector2 direction;
     PlayerEat player_eat;
+    public TextMeshProUGUI hpText;
 
     void Start()
     {
@@ -45,15 +49,15 @@ public class PlayerShoot : MonoBehaviour
     // create the bullet instance and bullet's velocity
     void Shoot()
     {
-        if (generator.created_bullet.Count >= 0)
-        {
-            mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            direction = (mouse_position - (Vector2)player.transform.position).normalized;
-            GameObject bullet_instance = Instantiate(bullet, bullet_start_point.position, player.transform.rotation);
-            Rigidbody2D rb = bullet_instance.GetComponent<Rigidbody2D>();
-            rb.velocity = direction * bullet_speed;
-            has_shot = true;
-        }
+        mouse_position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mouse_position - (Vector2)player.transform.position).normalized;
+        GameObject bullet_instance = Instantiate(bullet, bullet_start_point.position, player.transform.rotation);
+        Rigidbody2D rb = bullet_instance.GetComponent<Rigidbody2D>();
+        //rb.velocity = direction * bullet_speed;
+        GameManager.instance.DeductHP(5);
+        Debug.Log("HP deducted by 5%");
+        hpText.text = "HP: " + GameManager.instance.playerHP.ToString();
+        has_shot = true;
 
     }
 }
